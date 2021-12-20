5db7dfc9e2cc028ab277ac5c81cf9d3f90e27a5a
michaelfebrianto <michaelfebrianto@gmail.com>
2021-12-20 16:37:33 +1100

    APP-3276 : update embed v2

--- qrcode/android/src/main/kotlin/com/example/qrcode/QrcodePlugin.kt
+++ qrcode/android/src/main/kotlin/com/example/qrcode/QrcodePlugin.kt
@@ -1,7 +1,30 @@
 package com.example.qrcode
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class QrcodePlugin {
+class QrcodePlugin: FlutterPlugin, MethodCallHandler  {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "hello")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
