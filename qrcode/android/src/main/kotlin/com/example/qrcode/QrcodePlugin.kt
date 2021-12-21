30d618eb4e847c066b68040d26c54a31e071199c
michaelfebrianto <michaelfebrianto@gmail.com>
2021-12-21 22:22:34 +1100

    APP-3276 : update gradle

--- qrcode/android/src/main/kotlin/com/example/qrcode/QrcodePlugin.kt
+++ qrcode/android/src/main/kotlin/com/example/qrcode/QrcodePlugin.kt
@@ -9,14 +9,10 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 class QrcodePlugin: FlutterPlugin, MethodCallHandler  {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "hello")
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "plugins/qr_capture_view")
     channel.setMethodCallHandler(this)
   }
 
